6687c2ca0984fe0c5ee32353d0727d30f505bc01
DishaJeevan <dishajeevan67@gmail.com>
2026-04-09 19:42:24 +0530

    Update ManageOrder.jsx

--- my-react-app/src/admin/component/ManageOrder.jsx
+++ my-react-app/src/admin/component/ManageOrder.jsx
@@ -71,7 +71,7 @@ function ManageOrder() {
                 <td>
                   <button className="edit-btn" onClick={() => {
                     console.log("Editing user:", o._id);
-                   navigate(`/admin/user-orders/${o._id}`);
+                   navigate(`/admin/user-orders/${o.user_id}`);
                   }}><i className="fas fa-edit"></i>
                 </button>
 
