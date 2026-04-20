94fc97689de878513c7a82f1ba50e1e9ac4e7e77
DishaJeevan <dishajeevan67@gmail.com>
2026-04-20 15:31:39 +0530

    Update GreenStrip.jsx

--- my-react-app/src/components/GreenStrip.jsx
+++ my-react-app/src/components/GreenStrip.jsx
@@ -4,9 +4,7 @@ import "../App.css";
 function GreenStrip() {
   return (
     <div className="green-strip">
-      <Link to="/sell">Sell on BigHaat</Link>
-      <Link to="/bulk-orders">Bulk Order Inquiries</Link>
-      <Link to="/corporate">Corporate Site</Link>
+     
       <span className="call-btn">
         <Link to="/call-btn">Missed Call To Order: 1800-3000-2434</Link>
       </span>
