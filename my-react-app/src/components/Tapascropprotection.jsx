38b4f4247846b705462aafd2d0fdecd62d8b2804
DishaJeevan <dishajeevan67@gmail.com>
2026-04-20 15:41:18 +0530

    Update Tapascropprotection.jsx

--- my-react-app/src/components/Tapascropprotection.jsx
+++ my-react-app/src/components/Tapascropprotection.jsx
@@ -25,95 +25,7 @@ function Tapascropprotection () {
   <div className="tapasprotection-page">
     <div className="main-container">
 
-      <div className="sidebar">
-        <h2>Filters</h2>
-
-        <details open>
-          <summary>CATEGORIES
-            <i class="fa-solid fa-angle-down"></i>
-          </summary>
-          <div className="content">
-            <label><input type="checkbox" /></label>    
-          </div>
-        </details>
-
-        <details>
-          <summary>BRANDS
-            <i class="fa-solid fa-angle-down"></i>
-          </summary>
-          <div className="content">
-            <label><input type="checkbox" /> Adama</label>
-            <label><input type="checkbox" /> AgroHaat</label>
-            <label><input type="checkbox" /> AIMCO PESTICIDES LTD</label>
-            <label><input type="checkbox" /> Amruth Organic</label>
-            <label><input type="checkbox" /> BASF</label>
-          </div>
-        </details>
-        <details>
-            <summary>
-                PRICE
-                <i class="fa-solid fa-angle-down"></i>
-            </summary>
-            <div class="content">
-                <input type="range" min="0" max="20000" class="range"/>
-                <div class="price-box">
-                    <select>
-                        <option>₹0</option>
-                        <option>₹1000</option>
-                        <option>₹5000</option>
-                    </select>
-                    to
-                    <select>
-                        <option>₹20000</option>
-                        <option>₹15000</option>
-                        <option>₹10000</option>
-                    </select>
-                </div>
-            </div>
-        </details>
-
-        <details>
-          <summary>RATING
-            <i class="fa-solid fa-angle-down"></i>
-          </summary>
-          <div className="content">
-            <label><input type="checkbox" /> 4 ⭐ and above</label>
-            <label><input type="checkbox" /> 3 ⭐ and above</label>
-            <label><input type="checkbox" /> 2 ⭐ and above</label>
-            
-          </div>
-        </details>
-
-        <details>
-          <summary>AVAILABILITY
-            <i class="fa-solid fa-angle-down"></i>
-          </summary>
-          <div className="content">
-            <label><input type="checkbox" /> In Stock</label>
-            <label><input type="checkbox" /> Out of Stock</label>
-          </div>
-        </details>
-      </div>
-
-
-     
-      <div className="content-area">
-
-        <div className="page-header">
-          <div className="title-section">
-            <h1>Tapas Crop Protection </h1>
-          </div>
-
-          <div className="sort-section">
-            <div className="sort-box">
-              <span>Sort By:</span>
-              <select >
-                <option>Best Selling</option>
-                <option>Price Low to High</option>
-                <option>Price High to Low</option>
-              </select>
-            </div>
-          </div>
+      
          </div>
 
         <div className="products-grid-menu">
@@ -161,18 +73,9 @@ function Tapascropprotection () {
           </div>
           </div>
 
-          <div class="description">
-            <h3>Crop Protection</h3>
-            <p>
-              Products are used to kill and control all harmful organisms during the cultivation 
-              and to improve the yield and the quality of their crop growth. It contains one or more 
-              active substances and other co-formulated can be either biological or chemical products. 
-              Bio and Organic Plant protection products are available with various top brands.
-            </p>
-          </div>
+         
       </div>
-    </div>
-    </div>
+   
   );
 }
 
