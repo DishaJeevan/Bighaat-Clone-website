36ef51e5d423c501fb15ea303f156a6e61317f91
DishaJeevan <dishajeevan67@gmail.com>
2026-04-21 13:01:23 +0530

    Create About.jsx

---
+++ my-react-app/src/components/About.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const About = () => {
+  return (
+   <section class="about-container">
+  <div class="about-content">
+    
+    <div class="header-group">
+      <h2 class="main-heading">WHO WE ARE</h2>
+      <h3 class="tagline">"Transforming the Future of Farmers"</h3>
+    </div>
+
+    <div class="text-body">
+      <p>
+        BigHaat is founded by a team of avid entrepreneurs in the year 2015. 
+        BigHaat is India's leading Agri Digital Platform transforming the 
+        agriculture value chain from pre-harvest to post-harvest leveraging 
+        science, data and technology. It is impacting millions of farmers 
+        across the country by providing accessibility of extensive range of 
+        high quality inputs, end to end crop guidance and market linkages 
+        for various commodities, thus offering 360 degree solution to 
+        farmers with a very unique approach.
+      </p>
+
+      <p>
+        BigHaat Technology Platform is driving efficiencies of Agri Inputs 
+        manufacturers in the areas of distribution, marketing and operations 
+        with data-driven business intelligence. Our data strategy enables 
+        various stakeholders of Agri value chain to come together and build 
+        end to end ecosystem for farming community and driving sustainable 
+        agriculture.
+      </p>
+
+      
+    </div>
+  </div>
+
+</section>
+  );
+};
+
+export default About;
