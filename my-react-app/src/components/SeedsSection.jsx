f99ef327183a71bf92045fe2a77de7609cfb33e3
DishaJeevan <dishajeevan67@gmail.com>
2026-04-24 09:15:07 +0530

    Update SeedsSection.jsx

--- my-react-app/src/components/SeedsSection.jsx
+++ my-react-app/src/components/SeedsSection.jsx
@@ -9,11 +9,9 @@ function SeedsSection() {
   const getSeeds = async () => {
     try {
       const res = await axios.get("https://bighaat-clone.onrender.com/products");
-
       const filtered = res.data.filter(
         (p) => p.category === "seeds"
       );
-
       setSeedProducts(filtered);
     } catch (err) {
       console.error(err);
@@ -26,46 +24,44 @@ function SeedsSection() {
     <>
 
     <section class="seeds">
-            <div class="offers">
-                <h1>Seeds</h1>
-                <p>Quality Seeds, Proven Results</p>
-            </div>
-            <div class="offer-view-all">
-                <Link to="/Seedsmenue">View All</Link>
-            </div>
-        </section>
-<section className="card-seed">
-      {seedProducts.slice(0, 5).map((product) => (
-        <Link to={`/product/${product.id}`}  key={product.id} className="product-link">
-          <div className="product-card" key={product.id}>
-            <div className="discount">{product.discount}% Off</div>
-
-            <div className="image-offer">
-              <img src={product.image} />
-            </div>
-             <div className="rating-star-menu">
-                  ₹{product.oldPrice} <i className="fa-solid fa-arrow-right"></i> ₹{product.newPrice}
-             </div>
-
-            <div className="content-card">
-              <h4>{product.name}</h4>
-              <p className="brand">{product.brand}</p>
-
-              <div className="price">
-                <span className="new-price">₹{product.newPrice}</span>
-                <span className="old-price">₹{product.oldPrice}</span>
-              </div>
-
-              <p className="save-button">Save ₹{product.saveAmount}</p>
-            </div>
-
-            
+        <div class="offers">
+            <h1>Seeds</h1>
+            <p>Quality Seeds, Proven Results</p>
+         </div>
+         <div class="offer-view-all">
+            <Link to="/Seedsmenue">View All</Link>
           </div>
-          </Link>
-        ))}
-      </section>
-      </>
-      );
+        </section>
+        <section className="card-seed">
+            {seedProducts.slice(0, 5).map((product) => (
+              <Link to={`/product/${product.id}`}  key={product.id} className="product-link">
+                <div className="product-card" key={product.id}>
+                  <div className="discount">{product.discount}% Off</div>
+      
+                  <div className="image-offer">
+                    <img src={product.image} />
+                  </div>
+                   <div className="rating-star-menu">
+                        ₹{product.oldPrice} <i className="fa-solid fa-arrow-right"></i> ₹{product.newPrice}
+                   </div>
+      
+                  <div className="content-card">
+                    <h4>{product.name}</h4>
+                    <p className="brand">{product.brand}</p>
+      
+                    <div className="price">
+                      <span className="new-price">₹{product.newPrice}</span>
+                      <span className="old-price">₹{product.oldPrice}</span>
+                    </div>
+      
+                    <p className="save-button">Save ₹{product.saveAmount}</p>
+                  </div>                
+                </div>
+                </Link>
+              ))}
+            </section>
+       </>
+);
 }
 
 export default SeedsSection;
